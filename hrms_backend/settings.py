0ce0afec33c4f315ba7e2613b47263297646f4cf
uma-billiontags <uma@billiontags.co>
2026-08-13 13:13:36 +0530

    Repeats Daily Added

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -115,9 +115,10 @@ WSGI_APPLICATION = 'hrms_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('SQLITE_DB_PATH', BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
